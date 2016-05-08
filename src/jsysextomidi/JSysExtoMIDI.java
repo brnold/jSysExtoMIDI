bc17c75358175e506c7be923177c240bc7b64464
Benjamin Nold <brnold@oakland.edu>
2016-05-08 10:02:31 -0400

    Working midi "receiver," now to make it work.

--- src/jsysextomidi/JSysExtoMIDI.java
+++ src/jsysextomidi/JSysExtoMIDI.java
@@ -7,11 +7,11 @@ package jsysextomidi;
 
 import java.io.IOException;
 import java.util.LinkedList;
-import javax.sound.midi.MidiDevice;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Receiver;
-import javax.sound.midi.Transmitter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.midi.*;
+import static javax.sound.midi.ShortMessage.NOTE_OFF;
+import static javax.sound.midi.ShortMessage.NOTE_ON;
 
 /**
  *
@@ -23,7 +23,7 @@ public class JSysExtoMIDI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        MidiDevice device = null;
+        MidiDevice device = null, softwarePort = null;
         MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
         
         //myGUI theGUI = new myGUI(midiDevicetoString(infos));
@@ -36,39 +36,64 @@ public class JSysExtoMIDI {
         System.out.println(infos[2].getDescription());
         
         try{
-            device = MidiSystem.getMidiDevice(infos[1]);
-            device.open();
+            //device = MidiSystem.getMidiDevice(infos[1]);
+            //device.open();
+            
+            softwarePort = MidiSystem.getMidiDevice(infos[1  ]);
+            softwarePort.open();
         } catch (MidiUnavailableException e){
-            System.out.println("Hey, the device is unavailble!");
+            System.out.println("Hey, the devices are unavailble!");
         }
         
-        Receiver	r = new DumpReceiver(System.out);
+       // Receiver r = new myMidiReceiver(System.out);
+        
+        
         try
-                {
-                        Transmitter	t = device.getTransmitter();
-                        t.setReceiver(r);
+        {
+            //Transmitter tCord = device.getTransmitter(); //Make a Transmitter from my Midi Cord
+           // tCord.setReceiver(r); //Set the receiver to my class file
+            
+            Receiver receiverSoftware = softwarePort.getReceiver();
+            
+            ShortMessage m;
+            for(int i = 0; i < 127; i++){
+                
+                try {
+                    m = new ShortMessage(NOTE_ON, 13, i, 127);
+                    receiverSoftware.send(m, -1);
+                    Thread.sleep(125);
+                    m = new ShortMessage(NOTE_OFF, 13, i, 127);
+                } catch (InvalidMidiDataException ex) {
+                    System.out.println("Blah Blah data bo0!");
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(JSysExtoMIDI.class.getName()).log(Level.SEVERE, null, ex);
                 }
+             
+            }
+        }
         catch (MidiUnavailableException e)
                 {
                         System.out.println("wasn't able to connect the device's Transmitter to the Receiver:");
                         System.out.println(e); 
-                        device.close();
+                        //device.close();
                         System.exit(1);
                 }
         
         try
-			{
-				System.in.read();
-			}
-		catch (IOException ioe)
-			{
-			}
-		device.close();
-		System.out.println("Received "+((DumpReceiver) r).seCount+" sysex messages with a total of "+((DumpReceiver) r).seByteCount+" bytes");
-		System.out.println("Received "+((DumpReceiver) r).smCount+" short messages with a total of "+((DumpReceiver) r).smByteCount+" bytes");
-		System.out.println("Received a total of "+(((DumpReceiver) r).smByteCount + ((DumpReceiver) r).seByteCount)+" bytes");
+            {
+                
+                System.in.read();
+                System.out.print("Test");
+            }
+    catch (IOException ioe)
+            {
+            }
+		//device.close();
+		//System.out.println("Received "+((DumpReceiver) r).seCount+" sysex messages with a total of "+((DumpReceiver) r).seByteCount+" bytes");
+		//System.out.println("Received "+((DumpReceiver) r).smCount+" short messages with a total of "+((DumpReceiver) r).smByteCount+" bytes");
+		//System.out.println("Received a total of "+(((DumpReceiver) r).smByteCount + ((DumpReceiver) r).seByteCount)+" bytes");
         
-        device.close();
+            device.close();
         
     }
     
--- src/jsysextomidi/myMidiReceiver.java
+++ src/jsysextomidi/myMidiReceiver.java
@@ -6,6 +6,8 @@
 package jsysextomidi;
 
 import java.io.PrintStream;
+import java.util.Iterator;
+import java.util.LinkedList;
 import	javax.sound.midi.MidiSystem;
 import	javax.sound.midi.InvalidMidiDataException;
 import	javax.sound.midi.Sequence;
@@ -25,89 +27,133 @@ public class myMidiReceiver
 implements	Receiver
 {
     private PrintStream		m_printStream;
-    String sysExMessage;
+    String sysExMessageStr;
+    LinkedList<Byte> sysExBytes = null, oldMessageBytes= null; 
+
     
     public myMidiReceiver(PrintStream printStream)
     {
         m_printStream = printStream;
     }
     
+    
     @Override
     public void send(MidiMessage message, long timeStamp) {
        String strMessage = null;
+       
+       if(message instanceof ShortMessage){
+           //this.send(decodeMessage((ShortMessage) message));
+       }
        
        if (message instanceof SysexMessage){
           strMessage = decodeMessage((SysexMessage) message);
        }
        
        if(strMessage == "NotYet"){
-           return;
        }else{
-       m_printStream.println(strMessage);
+       //m_printStream.println(strMessage);
        }
+    }
+    
+    public void decodeMessage(ShortMessage message)
+    {
+        
     }
     
     public String decodeMessage(SysexMessage message)
     {
         byte[]	abData = message.getData();
-        int lenghtMessage = abData.length;
+        String messageHex = getHexString(abData);
+        int lenghtMessage = messageHex.length();
         String	strMessage = null;
-        // System.out.println("sysex status: " + message.getStatus());
+        //System.out.println("sysex status: " + message.getStatus());
+
         if (message.getStatus() == SysexMessage.SYSTEM_EXCLUSIVE)
         {
-            sysExMessage = getHexString(abData);
             strMessage = "Sysex message: F0" + getHexString(abData);
+            sysExMessageStr = getHexString(abData);
+            sysExBytes = new LinkedList<Byte>();
+            for(int i = 0; i < abData.length; i++){
+                sysExBytes.addLast(abData[i]);
+            }
         }
         else if (message.getStatus() == SysexMessage.SPECIAL_SYSTEM_EXCLUSIVE)
         {
-                //strMessage = "Continued Sysex message F7" + getHexString(abData);
-                strMessage = getHexString(abData);
-                sysExMessage += getHexString(abData);
+            //strMessage = "Continued Sysex message F7" + getHexString(abData);
+            //strMessage = getHexString(abData);
+            sysExMessageStr = sysExMessageStr + getHexString(abData);
+            for(int i = 0; i < abData.length; i++){
+                sysExBytes.addLast(abData[i]);
+            }
         }
 
 
-        if((abData[lenghtMessage-1]==15) &&(abData[lenghtMessage-2]==7)){
-            return sysExMessage;
+        if((messageHex.charAt(lenghtMessage-2)=='F') &&(messageHex.charAt(lenghtMessage-1)=='7')){
+            sysexToStop(sysExBytes);
+            return sysExMessageStr;
         }
         else return "NotYet";
     }
 
     
-    private static String sysexToStop(String message){
+    private void sysexToStop(LinkedList<Byte> message){
+        
+        //System.out.println(message.size());
+        System.out.println("------------------Start of new Data set----------");
+        if((message.get(14) & 0xF0) == 0x10){
+            System.out.println("Bourdon 8");
+        }else {
+            System.out.println("Bourdon 8 off");
+        }
+        
+        if((message.get(14) & 0x0F) == 1){
+            System.out.println("Viola 8");
+        }else{
+            System.out.println("Viola 8 off");
+        }
         
         
+    }
+    
+    //decrept, ha I got to say that finally
+    private static void sysexToStop(String message){
         
-        return "Stop Showing Red!"
+        //should check and see if the message lengths are the correct size
+        
+        if(message.charAt(0) == ' '){
+            System.out.println("Principal 16");
+        }
+        
+        
     }
     
     
-    
-    
+   
     
     @Override
     public void close() {
     }
     
     private static char hexDigits[] = 
-	   {'0', '1', '2', '3', 
-	    '4', '5', '6', '7', 
-	    '8', '9', 'A', 'B', 
-	    'C', 'D', 'E', 'F'};
+        {'0', '1', '2', '3', 
+         '4', '5', '6', '7', 
+         '8', '9', 'A', 'B', 
+         'C', 'D', 'E', 'F'};
     
     public static String getHexString(byte[] aByte)
-	{
-		StringBuffer	sbuf = new StringBuffer(aByte.length * 3 + 2);
-		for (int i = 0; i < aByte.length; i++)
-		{
-			sbuf.append(' ');
-			sbuf.append(hexDigits[(aByte[i] & 0xF0) >> 4]);
-			sbuf.append(hexDigits[aByte[i] & 0x0F]);
-			/*byte	bhigh = (byte) ((aByte[i] &  0xf0) >> 4);
-			sbuf.append((char) (bhigh > 9 ? bhigh + 'A' - 10: bhigh + '0'));
-			byte	blow = (byte) (aByte[i] & 0x0f);
-			sbuf.append((char) (blow > 9 ? blow + 'A' - 10: blow + '0'));*/
-		}
-		return new String(sbuf);
-	}
+    {
+        StringBuffer	sbuf = new StringBuffer(aByte.length * 3 + 2);
+        for (int i = 0; i < aByte.length; i++)
+        {
+                sbuf.append(' ');
+                sbuf.append(hexDigits[(aByte[i] & 0xF0) >> 4]);
+                sbuf.append(hexDigits[aByte[i] & 0x0F]);
+                /*byte	bhigh = (byte) ((aByte[i] &  0xf0) >> 4);
+                sbuf.append((char) (bhigh > 9 ? bhigh + 'A' - 10: bhigh + '0'));
+                byte	blow = (byte) (aByte[i] & 0x0f);
+                sbuf.append((char) (blow > 9 ? blow + 'A' - 10: blow + '0'));*/
+        }
+        return new String(sbuf);
+    }
     
 }
